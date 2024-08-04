5d6162496416870de7b9366a03226fe8f13b0090
Franklin Souza <fraank@riseup.net>
2024-08-04 16:41:30 -0300

    Commit automático no repositório mydots!

--- .config/polybar/scripts/updates-arch.sh
+++ .config/polybar/scripts/updates-arch.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
-    updates=0
+# Verificar atualizações dos repositórios oficiais
+if ! updates_official=$(checkupdates 2> /dev/null | wc -l); then
+    updates_official=0
 fi
 
-if [ "$updates" -gt 0 ]; then
-    echo "  $updates "
+# Verificar atualizações do AUR
+if ! updates_aur=$(paru -Qua 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+# Exibir o número de atualizações disponíveis
+if [ "$updates_official" -gt 0 ] || [ "$updates_aur" -gt 0 ]; then
+    echo "  $updates_official/$updates_aur "
 else
     echo "  No updates"
 fi
+
